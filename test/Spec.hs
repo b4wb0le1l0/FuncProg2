89535f21188e5afb601373b714931043c986464c
Egor <agasha3000@gmail.com>
2024-12-13 18:57:13 +0300

    added gitignore

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import qualified PropertyTestRBTree (properties)
+import Test.Tasty
+import qualified TestDict (tests)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup
+    "All tests"
+    [ TestDict.tests,
+      PropertyTestRBTree.properties
+    ]
